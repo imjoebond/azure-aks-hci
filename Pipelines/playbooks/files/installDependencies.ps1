bb0fef4bddfba3bd99bf60363d9605d662ab6ff5
Joe Bond <imjoebond@gmail.com>
2022-06-02 09:38:27 -0400

    source variables and use jinja2 instead of win_copy

--- Pipelines/playbooks/files/installDependencies.ps1
+++ Pipelines/playbooks/files/installDependencies.ps1
@@ -1,4 +1,4 @@
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Install-PackageProvider -Name NuGet -Force 
-Install-Module -Name PowershellGet -Force
-Install-Module -Name AksHci -Repository PSGallery -AcceptLicense -Force
+Install-PackageProvider -Name NuGet -Force -Confirm:$false
+Install-Module -Name PowershellGet -Force -Confirm:$false
+Install-Module -Name AksHci -Repository PSGallery -Force -Confirm:$false

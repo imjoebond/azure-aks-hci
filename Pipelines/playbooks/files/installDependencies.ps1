d97a976d039fba80f0604d8069bf0f71ec27f0c4
Joe Bond <imjoebond@gmail.com>
2022-06-02 10:11:35 -0400

    accept license

--- Pipelines/playbooks/files/installDependencies.ps1
+++ Pipelines/playbooks/files/installDependencies.ps1
@@ -1,4 +1,4 @@
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Install-PackageProvider -Name NuGet -Force -Confirm:$false
 Install-Module -Name PowershellGet -Force -Confirm:$false
-Install-Module -Name AksHci -Repository PSGallery -Force -Confirm:$false
+Install-Module -Name AksHci -Repository PSGallery -AcceptLicense -Force -Confirm:$false

38ad8731de8d92d95a8ce558fa585981a3acce04
Joe Bond <imjoebond@gmail.com>
2022-06-02 14:51:51 -0400

    refactor

--- Pipelines/playbooks/files/installDependencies.ps1
+++ Pipelines/playbooks/files/installDependencies.ps1
@@ -1,4 +1,4 @@
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Install-PackageProvider -Name NuGet -Force -Confirm:$false
 Install-Module -Name PowershellGet -Force -Confirm:$false
-Install-Module -Name AksHci -Repository PSGallery -AcceptLicense -Force -Confirm:$false
+#needs a new window Install-Module -Name AksHci -Repository PSGallery -AcceptLicense -Force -Confirm:$false
